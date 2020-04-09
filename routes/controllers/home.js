49139597de2b4abc670461a27a750e15413fd1a9
Bishal Bista <b3hal9@gmail.com>
2020-04-10 01:03:19 +0545

    fixing server

--- routes/controllers/home.js
+++ routes/controllers/home.js
@@ -2,6 +2,6 @@
 const keys = require('../../config/keys');
 
 module.exports = Home = (req,res)=>{
-    res.render('index',{stripePublishableKey: keys.StripePublishableKey});
+    res.render('index',{stripePublishableKey: keys.stripePublishableKey});
 };
 
--- routes/controllers/payment.js
+++ routes/controllers/payment.js
@@ -1,16 +1,18 @@
+const keys = require('../../config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
 const payment = (req,res)=>{
     const amount=2000;
     stripe.customers.create({
         email:req.body.stripeEmail,
         source:req.body.stripeToken
     })
-        .then(customer=>stripe.charges.create({
-            amount,
-            description:'Cooliest T-Merch in Planet.',
-            currency: 'usd',
-            customer:customer.id
-        }))
-        .then(res=>res.render('success'));
-    }
+    .then(customer=>stripe.charges.create({
+        amount,
+        description:'Cooliest T-Merch in Planet.',
+        currency: 'usd',
+        customer:customer.id
+    }))
+    .then(data=>res.render('success',{data:data}));
+}
 
     module.exports = payment;
